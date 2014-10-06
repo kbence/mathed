a0522803ea4d3fcc4de676d374064e3b5e814471
Bence Kiglics <bence.kiglics@gmail.com>
2014-10-06 22:11:18 +0200

    patching db automatically on provision

--- chef/cookbooks/sites/recipes/mathed.rb
+++ chef/cookbooks/sites/recipes/mathed.rb
@@ -2,3 +2,11 @@ apache_vhost do
   host 'mathed.dev'
   public_root '/var/www/mathed/webroot'
 end
+
+# db patching
+
+execute 'patch database' do
+  command <<-EOF
+    cd /var/www/mathed && php tools/dbpatcher/dbpatcher.php
+  EOF
+end
