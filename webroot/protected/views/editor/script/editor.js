4f9ce40e916593014c74767665a8da2569976141
Toco25 <poool21@freemail.hu>
2014-10-08 21:06:49 +0200

    Revert "Revert "divider""
    This reverts commit 3ef52c05c1890e0566ab90a7792277c207fc631a.

---
+++ webroot/protected/views/editor/script/editor.js
@@ -0,0 +1,9 @@
+<script language="javascript">
+<!--
+$(function() {
+	var editor = ace.edit('editor');
+	editor.setTheme('ace/theme/dawn');
+	editor.getSession().setMode('ace/mode/tex');
+});
+// -->
+</script>
