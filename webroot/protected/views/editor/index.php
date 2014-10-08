3ef52c05c1890e0566ab90a7792277c207fc631a
Toco25 <poool21@freemail.hu>
2014-10-08 21:06:34 +0200

    Revert "divider"
    This reverts commit 3125ea61e93116bc4ebb0bd1aa42221d5796e0e2.

--- webroot/protected/views/editor/index.php
+++ webroot/protected/views/editor/index.php
@@ -1,34 +1,30 @@
-﻿<?php
+<?php
 /* @var $this SiteController */
+
 $this->pageTitle = Yii::app()->name;
 Yii::app()->clientScript->registerCoreScript('jquery');
-include 'style/default.css';
-include 'style/editor.css';
-include 'script/default.js';
 ?>
-Editor
-  <div id="wl_document" onMouseUp = "moveDividerOut()">
-    <div id="wl_editor"> 
-	<pre id="editor">\begin{page}
-A kies honunkban leledzõ temérdek félelmetes bestia és szörny-állat között 
-aligha akad csodásabb és halálosabb fajzat, mint a Baziliskus, más néven 
-a Kígyók Királya. Ezen csúszómászó, mely gyakorta gigantikus méreteket ölt, 
-száz-évekig is elélhetik. Bölcsõje a tyúknak tojása, melyen varangyos béka kotlik. 
-Gyilkos fegyvere is felettébb csodásabb, hisz' halálos méregtõl csepegõ foga mellett 
-a nézése is halált hozó: valamely ember vagy állat tekintetének tüzébe kerül, nyomban 
-életét veszíti. A pókok menekülnek a Baziliskus elõl, mert emez gyûlölt ellenségük. 
-A Baziliskus pediglen féli a kakasnak rikkantását, minek hallatán menten kiszenved. 
-\end
-</pre>
 
-	<?php
-		include 'script/editor.js';
-	?>    
-    </div>
-    <div id="wl_divider" onMouseMove="moveDivider()" onmousedown="moveDividerClick()" onmouseup="moveDividerOut()"> </div>
-    <div id="wl_preview"> 
-        <img src="/images/bear.jpg" alt="bear" style="width:100%"/> 
-    </div>
-  </div>
+<h2>Edit page</h2>
 
+<style>
+    #editor {
+        margin: 0;
+        width: 100%;
+        height: 400px;
+    }
+</style>
 
+<pre id="editor">\begin{page}
+    Page content
+\end</pre>
+
+<script language="javascript">
+<!--
+$(function() {
+    var editor = ace.edit('editor');
+    editor.setTheme('ace/theme/dawn');
+    editor.getSession().setMode('ace/mode/tex');
+});
+// -->
+</script>
--- webroot/protected/views/editor/script/editor.js
+++
@@ -1,9 +0,0 @@
-<script language="javascript">
-<!--
-$(function() {
-	var editor = ace.edit('editor');
-	editor.setTheme('ace/theme/dawn');
-	editor.getSession().setMode('ace/mode/tex');
-});
-// -->
-</script>
