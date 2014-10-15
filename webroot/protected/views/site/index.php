1088a65eaf8c8efae8a45f17308a70263ef5a376
Toco25 <poool21@freemail.hu>
2014-10-15 22:15:51 +0200

    Create home page

--- webroot/protected/views/site/index.php
+++ webroot/protected/views/site/index.php
@@ -1,2 +1,27 @@
-<?php
-print "hihi";
+﻿<style>
+h1 {
+	color: #4682BF;
+	font: italic bold 30px Georgia, serif;
+}
+
+#index_style {
+	
+	color: #4682B4;
+	font: italic bold 14px Georgia, serif;
+    position: relative;
+    right: -10%;
+	width: 80%;
+}
+</style>
+
+<H1 align="center"> Matematikai szerkesztő portál</H1>
+
+<div id="index_style">
+	<p align="justify">
+		<?php print "Ezen portál célja olyan weboldal megalkotása, mely matematikai dokumentumok elkészítését támogatja és segít ezek közös felületen való kezelésében, megosztásában. A portál konkrét felhasználói igények alapján hiányt pótol. Tapasztalat szerint kevés olyan, jól működő weboldal létezik, melynek segítségével egy könnyen kezelhető szerkesztő felületen bevitt matematikai képleteket is tartalmazó TeX, vagy MathML kód alapján azonnal képet kaphatunk a lefordított forrásról. Az editor \"What You See Is What You Get\" (WYSIWYG) elvhez hasonló (fordítás időigényessége miatt nem valós idejű megjelenítés) módon működik. Lehetőség van az elkészült dokumentumoknak a felhasználó szintű kezelésére, jogok hozzárendelésével. Megosztott dokumentum esetén lehetőség van fórum topic hozzárendelésére, ahol a dokumentum készítője és bármely felhasználó, akinek van jogosultsága látni az adott forráskódot, kommenteket tud hozzáfűzni. ";?>
+	</p>
+</div>
+
+<p align="center">
+	<img src="/images/example.jpg" alt="" height="75%" width="75%" >
+</p>
