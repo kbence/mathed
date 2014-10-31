3336ed067bad9d67d869f728e8f06bdb20ee896b
Bence Kiglics <bence.kiglics@gmail.com>
2014-10-31 07:46:14 +0100

    moved editor toolbar code to a new file

--- webroot/protected/views/editor/edit.php
+++ webroot/protected/views/editor/edit.php
@@ -6,68 +6,15 @@ $clientScript = Yii::app()->clientScript;
 $clientScript->registerCoreScript('jquery');
 $clientScript->registerScriptFile('/js/divider.js');
 $clientScript->registerScriptFile('/js/editor.js', CClientScript::POS_END);
+$clientScript->registerScriptFile('/js/editor-toolbar.js', CClientScript::POS_END);
 $clientScript->registerCssFile('/css/divider.css');
 $clientScript->registerCssFile('/css/editor.css');
 ?>
 
 <script language="javascript">
-
-    <!--
+<!--
     var currentDocumentId = <?php echo $document['id'] ?>;
-	
-	$(document).ready(function(){
-		$(".button-comparsion span").click(function(){
-			var button_comparsion_position = $(".button-comparsion").position();		
-			$(".comparsion").css("visibility","visible"); 
-			$(".comparsion").css("top",button_comparsion_position.top+30+"px");	
-			$(".comparsion").css("left",button_comparsion_position.left+"px");			
-		});
-		$(".button-leq span").click(function(){
-			Editor.insert("\\leq");
-			$(".comparsion").css("visibility","hidden"); 
-		});
-		$(".button-geq span").click(function(){
-			Editor.insert("\\geq");
-			$(".comparsion").css("visibility","hidden"); 
-		});		
-		$(".button-prec span").click(function(){
-			Editor.insert("\\prec");
-			$(".comparsion").css("visibility","hidden"); 
-		});	
-		$(".button-succ span").click(function(){
-			Editor.insert("\\succ");
-			$(".comparsion").css("visibility","hidden"); 
-		});	
-		$(".button-triangleleft span").click(function(){
-			Editor.insert("\\triangleleft");
-			$(".comparsion").css("visibility","hidden"); 
-		});	
-		$(".button-triangleright span").click(function(){
-			Editor.insert("\\triangleright");
-			$(".comparsion").css("visibility","hidden"); 
-		});	
-		$(".button-neq span").click(function(){
-			Editor.insert("\\neq");
-			$(".comparsion").css("visibility","hidden"); 
-		});	
-		$(".button-equiv span").click(function(){
-			Editor.insert("\\equiv");
-			$(".comparsion").css("visibility","hidden"); 
-		});	
-		$(".button-approx span").click(function(){
-			Editor.insert("\\approx");
-			$(".comparsion").css("visibility","hidden"); 
-		});	
-		$(".button-cong span").click(function(){
-			Editor.insert("\\cong");
-			$(".comparsion").css("visibility","hidden"); 
-		});			
-		$(".button-propto span").click(function(){
-			Editor.insert("\\propto");
-			$(".comparsion").css("visibility","hidden"); 
-		});			
-	});
-    // -->
+// -->
 </script>
 
 Editor
