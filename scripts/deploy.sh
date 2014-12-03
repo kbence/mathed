4a3db3602dd308c180e377f1ff23b2c71f7705b7
Bence Kiglics <bence.kiglics@gmail.com>
2014-12-03 21:30:09 +0100

    added verbose output to tar

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ PACKAGE=$TEMP_DIR/mathed.tar.bz2
 wget http://jenkins.kiglics.hu/job/mathed-build/lastSuccessfulBuild/artifact/build/mathed.tar.bz2 -O $PACKAGE
 
 mkdir -p $TARGET_DIR
-(cd $TARGET_DIR && tar xfj $PACKAGE)
+(cd $TARGET_DIR && tar xfjv $PACKAGE)
 
 chef-solo --no-color -c /home/ubuntu/chef/solo.rb
 
