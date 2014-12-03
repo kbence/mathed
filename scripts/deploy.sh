82c31f9a5e981c7488d03bc16013c4ccf420f1d8
Bence Kiglics <bence.kiglics@gmail.com>
2014-12-03 21:05:20 +0100

    fixed deployment url

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ TEMP_DIR=$(mktemp -d)
 TARGET_DIR=/var/www/mathed
 PACKAGE=$TEMP_DIR/mathed.tar.bz2
 
-wget     http://jenkins.kiglics.hu/job/mathed-build/5/artifact/build/mathed.tar.bz2 -O $PACKAGE
+wget http://jenkins.kiglics.hu/job/mathed-build/lastSuccessfulBuild/artifact/build/mathed.tar.bz2 -O $PACKAGE
 
 mkdir -p $TARGET_DIR
 (cd $TARGET_DIR && tar xfj $PACKAGE)
