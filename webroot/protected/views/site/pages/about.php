2905ae970c7e315ecc05ad47d9aa86f707b988dd
Toco25 <poool21@freemail.hu>
2014-12-05 21:56:59 +0100

    about_v3

--- webroot/protected/views/site/pages/about.php
+++ webroot/protected/views/site/pages/about.php
@@ -18,40 +18,76 @@ h1 {
 
 <div id="index_style">
 	<p align="justify">
-		<?php print 
-"Regisztráció\n
-A felület lehetőséget ad új felhasználóként regisztrálni a rendszerbe.\n
-Ehhez felhasználó nevet, emailcímet, valamint jelszót kell megadni, majd a “Register” gombra kattintva a felhasználói profil létrejön a rendszerben.\n
-\n
-\n
-Login\n
-\n
-A Login felület a felhasználók beléptetését szolgálja a rendszerbe.\n
-    Csak előzetesen regisztrált felhasználóknak van módja a MathEd rendszerbe belépni, a regisztráció során megadott felhasználónév-jelszó párossal. \n
-\n
-A rendszer jelzi, ha a felhasználónév, vagy a jelszó nem megfelelő.\n
-\n
-\n
-Editor\n
-Dokumentumtár\n
-    A Dokumentumtár lehetőséget ad a korábban eltárolt dokumentumok megnyitására -a kiválasztott dokumentummal egy sorban lévő “Edit” gomb segítségével-, valamint új dokumentum hozzáadására, a “Create new” gomb segítségével. A rendszerbe belépett felhasználó csak az általa létrehozott dokumentumokat látja megjelenni a listában.\n
-\n
-Szerkesztő felület\n
-A szerkesztő ablak három fő részre oszlik. \n
-Bal oldalon, zölddel jelölten maga a forráskód bevitelére, szerkesztésére szolgáló felület található.\n
-Jobb oldalon, pirossal keretezve az előnézet helyezkedik el.\n
-Az sárgával jelölt területen a kommentálásra szolgáló beviteli mező, valamint a korábbi kommentek megjelenítésére szolgáló felület helyezkedik el.\n
-\n    
-\nElőnézet
-\nA dokumentum előnézetét az Editor ablak felső részén elhelyezkedő “Save & Preview” gomb segítségével lehet frissíteni. Ekkor a rendszer a forráskód változásait is menti.
-\n
-\n
-Comment felület\n
-A kommentek írására, megjelenítésére szolgáló eszköz a felhasználóknak nyújthat segítséget azzal, hogy a megosztott dokumentumok készítése során egymás munkáját interaktívan tudják segíteni. A felület alsó részén szövegbeviteli mezőbe írható egy új komment, feljebb a korábbi megjegyzések olvashatók, időbélyegzővel, valamint a kommentelő felhasználónevével ellátva.\n"
-		?>
+		 
+<?php print 
+"Regisztráció<br>
+<br>
+A felület lehetőséget ad új felhasználóként regisztrálni a rendszerbe. Ehhez felhasználó nevet, emailcímet, valamint jelszót kell megadni, majd a “Register” gombra kattintva a felhasználói profil létrejön a rendszerben.<br>
+<br>
+<br>"?>
+
+<p align="center">
+	<img src="/images/about_1.jpg" alt="" >
+</p>
+
+<?php print
+"Login<br>
+<br>
+A Login felület a felhasználók beléptetését szolgálja a rendszerbe. Csak előzetesen regisztrált felhasználóknak van módja a MathEd rendszerbe belépni, a regisztráció során megadott felhasználónév-jelszó párossal. <br>
+<br>";?>
+<p align="center">
+	<img src="/images/about_2.jpg" alt="" >
+</p>
+<?php print"
+A rendszer jelzi, ha a felhasználónév, vagy a jelszó nem megfelelő.<br>
+<br>
+";?>
+
+<p align="center">
+	<img src="/images/about_3.jpg" alt="" >
+</p>
+
+<?php print"
+<br>
+<br>
+Editor<br>
+Dokumentumtár<br>
+    A Dokumentumtár lehetőséget ad a korábban eltárolt dokumentumok megnyitására -a kiválasztott dokumentummal egy sorban lévő “Edit” gomb segítségével-, valamint új dokumentum hozzáadására, a “Create new” gomb segítségével. A rendszerbe belépett felhasználó csak az általa létrehozott dokumentumokat látja megjelenni a listában.<br>
+<br>";?>
+
+<p align="center">
+	<img src="/images/about_4.jpg" alt="" >
+</p>
+
+<?php print"
+Szerkesztő felület<br>
+A szerkesztő ablak három fő részre oszlik: <br>
+Bal oldalon, zölddel jelölten maga a forráskód bevitelére, szerkesztésére szolgáló felület található.<br>
+Jobb oldalon, pirossal keretezve az előnézet helyezkedik el.<br>
+Az sárgával jelölt területen a kommentálásra szolgáló beviteli mező, valamint a korábbi kommentek megjelenítésére szolgáló felület helyezkedik el.<br>
+<br>";?>
+
+<p align="center">
+	<img src="/images/about_5.jpg" alt="" >
+</p>
+
+<?php print"
+<br>Előnézet
+<br>A dokumentum előnézetét az Editor ablak felső részén elhelyezkedő “Save & Preview” gomb segítségével lehet frissíteni. Ekkor a rendszer a forráskód változásait is menti.
+<br>
+<br>";?>
+
+<p align="center">
+	<img src="/images/about_6.jpg" alt="" >
+</p>
+
+<?php print"
+Comment felület<br>
+A kommentek írására, megjelenítésére szolgáló eszköz a felhasználóknak nyújthat segítséget azzal, hogy a megosztott dokumentumok készítése során egymás munkáját interaktívan tudják segíteni. A felület alsó részén szövegbeviteli mezőbe írható egy új komment, feljebb a korábbi megjegyzések olvashatók, időbélyegzővel, valamint a kommentelő felhasználónevével ellátva.<br>
+";?>
 	</p>
 </div>
 
 <p align="center">
-	<img src="/images/example.jpg" alt="" height="75%" width="75%" >
+	<img src="/images/about_7.jpg" alt="" >
 </p>
