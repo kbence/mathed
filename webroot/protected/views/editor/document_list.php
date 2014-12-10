7029907857c8d78aed427436e9bbc955b1d55078
Bence Kiglics <bence.kiglics@gmail.com>
2014-12-10 06:06:42 +0100

    showing view for own documents as well

--- webroot/protected/views/editor/document_list.php
+++ webroot/protected/views/editor/document_list.php
@@ -6,9 +6,8 @@
             <td>
                 <?php if ($edit): ?>
                     <a href="?r=editor/edit&amp;id=<?php echo $doc->id ?>">Edit</a>
-                <?php else: ?>
-                    <a href="?r=editor/view&amp;id=<?php echo $doc->id ?>">View</a>
                 <?php endif ?>
+                <a href="?r=editor/view&amp;id=<?php echo $doc->id ?>">View</a>
             </td>
         </tr>
     <?php endforeach ?>
